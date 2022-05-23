e49fa3ecc9fb5e03b0b2a6bc4c2022af07816650
kostik-loyko <kostik.loyko@mail.ru>
2022-05-23 19:06:52 +0300

    feat: finished 05 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,12 +4,13 @@ const path = require('path');
 fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
    if (err) throw err;
 
+   let writeableStream = fs.createWriteStream(path.join(__dirname, 'project-dist/bundle.css'));
+
    files.forEach(file => {
       if (file.isFile() && path.extname(file.name) === '.css') {
          let readableStream = fs.createReadStream(path.join(__dirname, `styles/${file.name}`), 'utf-8');
-         let writeableStream = fs.createWriteStream(path.join(__dirname, 'project-dist/bundle.css'));
-         readableStream.on('data', data => writeableStream.write(data));
-         // readableStream.pipe(writeableStream);
+
+         readableStream.pipe(writeableStream);
       }
    });
 });
