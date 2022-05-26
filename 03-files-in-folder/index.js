8d1f565459d90977df0c3d496730a7b9fbdb6be0
kostik-loyko <kostik.loyko@mail.ru>
2022-05-26 08:52:57 +0300

    fix: fixed 03 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -11,9 +11,8 @@ const reader = (way) => {
 
          let newWay = path.join(way, file.name);
 
-         if (file.isDirectory()) {
-            reader(newWay);
-         } else {
+         if (file.isFile()) {
+            
             fs.stat(newWay, (err, stats) => {
                if (err) throw err;
                console.log(path.basename(newWay, path.extname(newWay)) + ' - ' + path.extname(file.name).slice(1) + ' - ' + ((stats.size) / 1000) + 'kb');
@@ -24,3 +23,30 @@ const reader = (way) => {
 }
 
 reader(way);
+
+
+// ДЛЯ ПРОХОЖДЕНИЯ ПО ВСЕМ ВЛОЖЕННЫМ ПАПКАМ
+
+// let way = path.join(__dirname, "secret-folder");
+
+// const reader = (way) => {
+//    fs.readdir(way, { withFileTypes: true }, (err, files) => {
+//       if (err) throw err;
+
+//       files.forEach(file => {
+
+//          let newWay = path.join(way, file.name);
+
+//          if (file.isDirectory()) {
+//             reader(newWay);
+//          } else {
+//             fs.stat(newWay, (err, stats) => {
+//                if (err) throw err;
+//                console.log(path.basename(newWay, path.extname(newWay)) + ' - ' + path.extname(file.name).slice(1) + ' - ' + ((stats.size) / 1000) + 'kb');
+//             });
+//          }
+//       })
+//    });
+// }
+
+// reader(way);
